235ebd2a4ec631d981a7413cbfd841cdcf5f4da3
Victor Tai <sunfong@gmail.com>
2025-08-29 17:40:57 -0700

    Initial Commit

---
+++ ReceiptOrganizer/ReceiptOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptOrganizerApp.swift
+//  ReceiptOrganizer
+//
+//  Created by Victor Tai on 2025-08-29.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
