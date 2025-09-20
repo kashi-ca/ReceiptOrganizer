45bba295d13e27307dabe4f54160743985c2ad60
Victor Tai <sunfong@gmail.com>
2025-09-20 08:06:47 -0700

    Add more samples

--- ReceiptOrganizer/Utilities/SampleImageProvider.swift
+++ ReceiptOrganizer/Utilities/SampleImageProvider.swift
@@ -9,11 +9,15 @@ import AppKit
 
 /// Utilities for supplying a sample receipt image during local testing.
 enum SampleImageProvider {
+    static let imageNames = ["receipt-ocr-original", "sample2", "sample3", "sample4"]
+    static var counter = 0
     /// Returns a generated sample receipt-like image as `CGImage`.
     /// Generated via Core Graphics to keep dependencies minimal.
     static func sampleReceiptImage() -> CGImage? {
         #if os(iOS)
-        return UIImage(named: "receipt-ocr-original")!.cgImage!
+        let index = counter % 4
+        counter += 1
+        return UIImage(named: imageNames[index])!.cgImage!
         #else
         if let image = NSImage(named: "receipt-ocr-original") {
             return image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
